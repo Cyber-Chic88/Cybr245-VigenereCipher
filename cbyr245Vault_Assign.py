95061eb1319268cf59887a9d6b68b259623c49f2
Cyber-Chic88 <113476677+Cyber-Chic88@users.noreply.github.com>
2022-11-09 11:26:39 -0500

    Update cbyr245Vault_Assign.py

--- cbyr245Vault_Assign.py
+++ cbyr245Vault_Assign.py
@@ -14,14 +14,15 @@ def read_secret(client):
     print(read_response)
     
 def create_key(client):
-    client.secrets.transit.create_key(name='my_python_key', key.type='aes256-gcm96')   #I noticed you used my_python_key instead
-                                                                                    ## of hvac-key so I used it throughout in place of hvac-key, should I've?
+    client.secrets.transit.create_key(name='my_python_key', key.type='aes256-gcm96')  
+                                                                                    
 def read_key(client):                                                                           
     read_key_response = client.secrets.transit.read_key(name='my_python_key')
     latest_version = read_key_response['data']['lastest_version']
     print('Latest version for key "my_python_key" is: , {ver}'.format(ver=latest_version))
 
-def base64ify():                            #Dont really know how to set this one up
+def base64ify():  
+    return
     
 def transit_encrypt(client, plain_text):
     #text_bytes = plain_text.encode("utf-8")
@@ -34,13 +35,13 @@ def transit_decrypt(client, ciphertext, decrypt_key="my.python_key"):
     decrypt_data_response = client.secrets.transit.decrypt_data(name=decrypt_key, ciphertext=ciphertext)
     return str(base64.b64decode(decrypt_data_response['data']['plaintext']), "utf-8")
 
-if __name__==__main__:                  #Am I setting my driver up correctly?
-    clt = hvac.Client(url='https://localhost:8200')
-    init_server(clt)
-    write_secret(clt)
-    read_secret(clt)
-    create_key(clt)
-    read_key(clt)
-    transit_encrypt(clt, "This is the plaintext to be encrypted")  #Would this be where I enter the plaintext to be encrypted?
-    transit_decrypt(clt, )                #Dont know what to enter for 2nd or 3rd parameter
-main()
+                
+ clt = hvac.Client(url='https://localhost:8200')
+ init_server(clt)
+ write_secret(clt)
+ read_secret(clt)
+ create_key(clt)
+ read_key(clt)
+ transit_encrypt(clt, "This is the plaintext to be encrypted")  
+ transit_decrypt(clt, )               
+
